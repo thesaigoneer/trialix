399b2d71226934b029c85c85ceaf8687b4e72c2f
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-21 15:12:16 +0700

    Update build.sh - try 3

--- build_files/build.sh
+++ build_files/build.sh
@@ -28,7 +28,7 @@ dnf5 install -y ptyxis
 dnf5 install -y zsh
 dnf5 install -y zsh-autosuggestions
 
-dnf5 install -y group 'Development Tools'
+dnf5 group install -y 'Development Tools'
 dnf5 install -y  procps-ng curl file git
 
 dnf5 remove -y firefox
