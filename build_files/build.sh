a09d7d3787034c21547db56540e2e00825001940
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-25 07:20:12 +0700

    Update build.sh - prep Cosmix

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,20 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y fastfetch
-dnf5 install -y gdm
-dnf5 install -y gnome-control-center
-dnf5 install -y gnome-session-wayland-session
-dnf5 install -y gnome-shell
-dnf5 install -y gvfs
-dnf5 install -y gvfs-archive
-dnf5 install -y gvfs-client
-dnf5 install -y gvfs-fuse
-dnf5 install -y gvfs-mtp
-dnf5 install -y gvfs-smb
 dnf5 install -y mc
-dnf5 install -y nautilus
-dnf5 install -y nautilus-extensions
-dnf5 install -y ptyxis
 dnf5 install -y zsh
 dnf5 install -y zsh-autosuggestions
 
@@ -32,8 +19,6 @@ dnf5 group install -y development-tools
 dnf5 install -y  procps-ng curl file git
 
 dnf5 remove -y firefox
-dnf5 remove -y gnome-tour
-dnf5 remove -y hplip
 dnf5 remove -y nvtop
 
 
